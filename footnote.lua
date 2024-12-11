458b0c0794490153300b5ff41024b08dc8b050e9
Fusyong <aahuaang@gmail.com>
2024-12-11 21:14:49 +0800

    dataset

--- footnote.lua
+++ footnote.lua
@@ -1,19 +1,26 @@
 
 \startluacode
 
-context("\\incrementuniquename")
-
 -- 定义lua函数，存储在给用户分配的userdata中
 userdata = userdata or { }
 function userdata.basefootnote(buf, page, uniquename)
+    local getdata = job.datasets.getdata
+    local setdata = job.datasets.setdata
     -- 解析位置参数
     local buf_option = utilities.parsers.settings_to_array(buf)[1]
     local page_option = utilities.parsers.settings_to_array(page)[1]
     -- 解析命名参数
     -- local named_values = utilities.parsers.settings_to_hash(keyvals)
+    --context("\\setdataset[footnotetable][%s][buf={%s},page={%s}]",uniquename,buf_option,page_option)
+    setdata {
+        name  = "footnotetable",
+        tag   = uniquename,
+        data  = {
+            ["uniquename"]=99999,
+        }
+    }
 
-    local getdata = job.datasets.getdata
-    local setdata = job.datasets.setdata
+
     -- 找到自己的真实页码
     local current_realpage
     local current_order
@@ -30,7 +37,7 @@ function userdata.basefootnote(buf, page, uniquename)
                         and current_realpage == getdata("footnotetable", n, "realpage")
                         and current_order > getdata("footnotetable", n, "order") then
                             local firstre = getdata("footnotetable", n, "uniquename")
-                            -- context("\\setdataset[footnotetable][buf={%s},page={%s},uniquename={%s},firstre={%s}]",buf_option,page_option,uniquename,firstre)
+                            context("\\setdataset[footnotetable][%s][buf={%s},page={%s},firstre={%s}]",uniquename,buf_option,page_option,firstre)
                             context("\\footnote{同[\\in{}{}[%s]]%s.}",firstre,page_option)
                         break
                     end
@@ -38,12 +45,13 @@ function userdata.basefootnote(buf, page, uniquename)
                 end
             break
         else
-            -- context("\\setdataset[footnotetable][buf={%s},page={%s},uniquename={%s}]",buf_option,page_option,uniquename)
+            context("\\setdataset[footnotetable][%s][buf={%s},page={%s}]",uniquename,buf_option,page_option)
             context("\\footnote[%s]{\\getbuffer[%s]\\unskip:%s.}",uniquename,buf_option,page_option)
         end
         i = i + 1
     end
 
+    -- \setdataset[footnotetable][buf={#1},page={#2},uniquename={\uniquename}]
 
 end
 
